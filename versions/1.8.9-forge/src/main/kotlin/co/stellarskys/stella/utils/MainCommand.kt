c612729d58f738fef126f283567ea3cb570236a0
eclipse-5214 <elipscomb5214@gmail.com>
2025-08-26 20:31:39 -0400

    messing around

---
+++ versions/1.8.9-forge/src/main/kotlin/co/stellarskys/stella/utils/MainCommand.kt
@@ -0,0 +1,9 @@
+package co.stellarskys.stella.utils
+
+import co.stellarskys.stella.Stella
+object MainCommand: CommandUtils(
+    "stella",
+    "Opens the Config",
+    listOf("sa", "sta")
+) {
+}
