21833581f17140ccaefb823c66dfd42c9e0c9827
Eclipse-5214 <elijahlipscomb55@gmail.com>
2025-09-02 15:44:12 -0400

    box wither doors done!

---
+++ src/main/kotlin/co/stellarskys/stella/utils/render/StellaRenderLayers.kt
@@ -0,0 +1,164 @@
+package co.stellarskys.stella.utils.render
+
+//#if MC >= 1.21.5
+
+//import co.stellarskys.stella.utils.render.layers.ChromaRenderLayer
+import it.unimi.dsi.fastutil.doubles.Double2ObjectMap
+import it.unimi.dsi.fastutil.doubles.Double2ObjectOpenHashMap
+import net.minecraft.client.gl.RenderPipelines
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.RenderLayer.MultiPhase
+import net.minecraft.client.render.RenderLayer.MultiPhaseParameters
+import net.minecraft.client.render.RenderPhase
+import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer
+import net.minecraft.util.Identifier
+import net.minecraft.util.TriState
+import net.minecraft.util.Util
+import java.util.OptionalDouble
+import java.util.function.DoubleFunction
+
+object StellaRenderLayers {
+    private val linesThroughWallsLayers: Double2ObjectMap<RenderLayer.MultiPhase> = Double2ObjectOpenHashMap()
+    private val chromaLinesLayer: Double2ObjectMap<RenderLayer.MultiPhase> = Double2ObjectOpenHashMap()
+    private val linesLayers: Double2ObjectMap<RenderLayer.MultiPhase> = Double2ObjectOpenHashMap()
+
+    /*
+    val CHROMA_STANDARD: MultiPhase = ChromaRenderLayer(
+        "stella_standard_chroma",
+        RenderLayer.CUTOUT_BUFFER_SIZE,
+        false,
+        false,
+        StellaRenderPipelines.CHROMA_STANDARD,
+        MultiPhaseParameters.builder().build(false)
+    )
+
+    private val CHROMA_TEXTURED: java.util.function.Function<Identifier, RenderLayer> = Util.memoize {
+            texture ->
+        ChromaRenderLayer(
+            "text_chroma",
+            RenderLayer.CUTOUT_BUFFER_SIZE,
+            false,
+            false,
+            StellaRenderPipelines.CHROMA_TEXT,
+            MultiPhaseParameters.builder()
+                .texture(RenderPhase.Texture(texture, TriState.FALSE, false))
+                .build(false)
+        )
+    }
+
+    val CHROMA_3D: MultiPhase = ChromaRenderLayer(
+        "standard_chroma",
+        RenderLayer.DEFAULT_BUFFER_SIZE,
+        false,
+        true,
+        StellaRenderPipelines.CHROMA_3D,
+        MultiPhaseParameters.builder()
+            .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
+            .build(false)
+    )
+
+    private val CHROMA_LINES = DoubleFunction { width ->
+        ChromaRenderLayer(
+            "chroma_lines",
+            RenderLayer.DEFAULT_BUFFER_SIZE, false, false,
+            StellaRenderPipelines.CHROMA_LINES,
+            MultiPhaseParameters.builder()
+                .lineWidth(RenderPhase.LineWidth(OptionalDouble.of(width)))
+                .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
+                .build(false)
+        )
+    }
+     */
+
+    private val LINES_THROUGH_WALLS = DoubleFunction { width ->
+        RenderLayer.of(
+            "lines_through_walls",
+            RenderLayer.DEFAULT_BUFFER_SIZE, false, false,
+            StellaRenderPipelines.LINES_THROUGH_WALLS,
+            MultiPhaseParameters.builder()
+                .lineWidth(RenderPhase.LineWidth(OptionalDouble.of(width)))
+                .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
+                .build(false)
+        )
+    }
+
+    private val LINES = DoubleFunction { width ->
+        RenderLayer.of(
+            "lines",
+            RenderLayer.DEFAULT_BUFFER_SIZE, false, false,
+            RenderPipelines.LINES,
+            MultiPhaseParameters.builder()
+                .lineWidth(RenderPhase.LineWidth(OptionalDouble.of(width)))
+                .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
+                .build(false)
+        )
+    }
+
+    val FILLED: RenderLayer.MultiPhase = RenderLayer.of(
+        "filled", RenderLayer.DEFAULT_BUFFER_SIZE, false, true,
+        RenderPipelines.DEBUG_FILLED_BOX,
+        RenderLayer.MultiPhaseParameters.builder()
+            .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
+            .build(false)
+    )
+
+
+    val FILLEDTHROUGHWALLS: RenderLayer.MultiPhase = RenderLayer.of(
+        "filled_through_walls", RenderLayer.DEFAULT_BUFFER_SIZE, false, true,
+        StellaRenderPipelines.FILLED_THROUGH_WALLS,
+        RenderLayer.MultiPhaseParameters.builder()
+            .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
+            .build(false)
+    )
+
+    val BEACON_BEAM_OPAQUE = RenderLayer.of(
+        "beacon_beam_opaque", 1536, false, true,
+        StellaRenderPipelines.BEACON_BEAM_OPAQUE,
+        RenderLayer.MultiPhaseParameters
+            .builder()
+            .texture(RenderPhase.Texture(BeaconBlockEntityRenderer.BEAM_TEXTURE, TriState.FALSE, false))
+            .build(false)
+    )
+
+    val BEACON_BEAM_OPAQUE_THROUGH_WALLS = RenderLayer.of(
+        "beacon_beam_opaque_through_walls", 1536, false, true,
+        StellaRenderPipelines.BEACON_BEAM_OPAQUE_THROUGH_WALLS,
+        RenderLayer.MultiPhaseParameters
+            .builder()
+            .texture(RenderPhase.Texture(BeaconBlockEntityRenderer.BEAM_TEXTURE, TriState.FALSE, false))
+            .build(false)
+    )
+
+    val BEACON_BEAM_TRANSLUCENT = RenderLayer.of(
+        "beacon_beam_translucent", 1536, false, true,
+        StellaRenderPipelines.BEACON_BEAM_TRANSLUCENT,
+        RenderLayer.MultiPhaseParameters
+            .builder()
+            .texture(RenderPhase.Texture(BeaconBlockEntityRenderer.BEAM_TEXTURE, TriState.FALSE, false))
+            .build(false)
+    )
+
+    val BEACON_BEAM_TRANSLUCENT_THROUGH_WALLS = RenderLayer.of(
+        "devonian_beacon_beam_translucent_esp",1536,false,true,
+        StellaRenderPipelines.BEACON_BEAM_TRANSLUCENT_THROUGH_WALLS,
+        RenderLayer.MultiPhaseParameters
+            .builder()
+            .texture(RenderPhase.Texture(BeaconBlockEntityRenderer.BEAM_TEXTURE, TriState.FALSE, false))
+            .build(false)
+    )
+
+    fun getLinesThroughWalls(width: Double): RenderLayer.MultiPhase =
+        linesThroughWallsLayers.computeIfAbsent(width, LINES_THROUGH_WALLS)
+
+    fun getLines(width: Double): RenderLayer.MultiPhase =
+        linesLayers.computeIfAbsent(width, LINES)
+
+    /*
+    fun getChromaLines(width: Double): RenderLayer.MultiPhase =
+        chromaLinesLayer.computeIfAbsent(width, CHROMA_LINES)
+
+    fun getChromaTextured(identifier: Identifier) = CHROMA_TEXTURED.apply(identifier)
+     */
+}
+
+//#endif
--- versions/1.8.9-forge/src/main/kotlin/co/stellarskys/stella/utils/MainCommand.kt
+++ versions/1.8.9-forge/src/main/kotlin/co/stellarskys/stella/utils/MainCommand.kt
@@ -5,6 +5,7 @@ import co.stellarskys.stella.Stella.Companion.mc
 import co.stellarskys.stella.hud.HUDEditor
 import co.stellarskys.stella.utils.CommandUtils
 import co.stellarskys.stella.utils.TickUtils
+import co.stellarskys.stella.utils.skyblock.dungeons.DungeonScanner
 import net.minecraft.command.ICommandSender
 import net.minecraft.util.BlockPos
 
@@ -34,5 +35,28 @@ object MainCommand: CommandUtils(
     override fun getTabCompletions(sender: ICommandSender, args: Array<String>, pos: BlockPos): List<String> {
         if (args.size == 1) return listOf("hud").filter { it.startsWith(args[0].lowercase()) }
         return emptyList()
+    }
+}
+
+@Stella.Command
+object dhdebug: CommandUtils(
+    "sadb",
+    "Opens the Config"
+) {
+    override fun processCommand(sender: ICommandSender?, args: Array<out String?>?) {
+        val room = DungeonScanner.currentRoom ?: return
+        val name = room.name ?: "Unnamed"
+        val cores = room.cores
+
+        if (cores.isEmpty()) {
+            ChatUtils.addMessage("${Stella.PREFIX} §b$name §fhas no scanned cores!")
+        } else {
+            ChatUtils.addMessage("${Stella.PREFIX} §b$name §fcore hash${if (cores.size > 1) "es" else ""}:")
+            cores.forEach {
+                ChatUtils.addMessage(" - $it")
+            }
+
+            ChatUtils.addMessage("${Stella.PREFIX} §b$name §ftype is §b${room.type}§f, with a checkmark of §b${room.checkmark}§f, explored? ${room.explored}")
+        }
     }
 }
