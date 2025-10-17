a48d2beb9e4161a98a8a55b29497e2e019cfaa8d
Eclipse-5214 <elijahlipscomb55@gmail.com>
2025-10-17 13:42:17 -0400

    forgot about this one

--- src/main/kotlin/co/stellarskys/stella/features/dungeons/roomName.kt
+++ src/main/kotlin/co/stellarskys/stella/features/dungeons/roomName.kt
@@ -7,22 +7,14 @@ import co.stellarskys.stella.utils.render.Render2D
 import co.stellarskys.stella.utils.skyblock.dungeons.Dungeon
 import co.stellarskys.stella.utils.skyblock.dungeons.DungeonScanner
 import co.stellarskys.stella.utils.CompatHelpers.UDrawContext
-//#if MC > 1.21.5
+import co.stellarskys.stella.utils.config
 import co.stellarskys.stella.events.GuiEvent
-//#elseif MC == 1.8.9
-//$$ import co.stellarskys.stella.events.RenderEvent
-//#endif
 
 @Stella.Module
 object roomName : Feature("showRoomName", area = "catacombs") {
     override fun initialize() {
         HUDManager.register("roomname", "No Room Found")
-
-        //#if MC > 1.21.5
         register<GuiEvent.HUD> { renderHUD(it.context)}
-        //#elseif MC == 1.8.9
-        //$$ register<RenderEvent.Text> { renderHUD(it.context) }
-        //#endif
     }
 
     private fun renderHUD(
@@ -31,7 +23,9 @@ object roomName : Feature("showRoomName", area = "catacombs") {
         if (!HUDManager.isEnabled("roomname")) return
         if (Dungeon.inBoss()) return
 
-        val text = "§z${DungeonScanner.currentRoom?.name ?: "No Room Found"}"
+        val chroma = config["roomNameChroma"] as Boolean
+
+        val text = "${if (chroma) "§z" else ""}${DungeonScanner.currentRoom?.name ?: "No Room Found"}"
         val x = HUDManager.getX("roomname") + 5f
         val y = HUDManager.getY("roomname") + 5f
         val scale = HUDManager.getScale("roomname")
