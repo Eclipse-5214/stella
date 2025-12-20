c40b11a6b70a545a9dec42b9e166442c50ab9072
eclipse-5214 <elipscomb5214@gmail.com>
2025-12-20 00:08:15 -0500

    command part

--- src/main/kotlin/co/stellarskys/stella/utils/Commodore.kt
+++ src/main/kotlin/co/stellarskys/stella/utils/Commodore.kt
@@ -4,7 +4,7 @@ import com.github.stivais.commodore.nodes.Executable
 import com.github.stivais.commodore.nodes.LiteralNode
 import com.mojang.brigadier.CommandDispatcher
 
-/* Modified version of commodor that works as an object :D */
+/* Modified version of commodore that works as an object :D */
 open class Commodore(val root: LiteralNode) {
     constructor(
         vararg name: String
@@ -13,24 +13,14 @@ open class Commodore(val root: LiteralNode) {
     /**
      * DSL access to the root node for object-style definitions.
      */
-    fun runs(function: Function<Unit>) = root.runs(function)
-
     fun runs(block: () -> Unit) = root.runs(block)
+    fun literal(string: String, block: LiteralNode.() -> Unit = {}): LiteralNode = root.literal(string, block)
+    fun literal(vararg names: String, block: LiteralNode.() -> Unit = {}): LiteralNode =
+        root.literal(*names, block = block)
 
-    fun literal(string: String, block: LiteralNode.() -> Unit = {}): LiteralNode {
-        return root.literal(string, block)
-    }
-
-    fun literal(vararg names: String, block: LiteralNode.() -> Unit = {}): LiteralNode {
-        return root.literal(*names, block = block)
-    }
-
-    fun executable(block: Executable.() -> Unit): Executable {
-        return root.executable(block)
-    }
+    fun executable(block: Executable.() -> Unit): Executable = root.executable(block)
 
     fun register(dispatcher: CommandDispatcher<*>) {
-        val cmd = com.github.stivais.commodore.Commodore(root)
-        cmd.register(dispatcher)
+        com.github.stivais.commodore.Commodore(root).register(dispatcher)
     }
 }
