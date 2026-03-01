1529193df05e1f43c9a8b5c94f62d0f4d58ce456
Eclipse-5214 <elijahlipscomb55@gmail.com>
2026-03-01 16:38:45 -0500

    Fix inventory buttons rendering behind inventory

--- src/main/java/co/stellarskys/stella/mixins/MixinContainer.java
+++ src/main/java/co/stellarskys/stella/mixins/MixinContainer.java
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractContainerScreen.class)
 public class MixinContainer {
-    @Inject(method = "renderBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/AbstractContainerScreen;renderBg(Lnet/minecraft/client/gui/GuiGraphics;FII)V"))
-    public void onRenderBackground(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci){
+    @Inject(method = "renderContents", at = @At("HEAD"))
+    public void onRenderContents(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci){
         EventBus.INSTANCE.post(new GuiEvent.Container.Content(guiGraphics, i, j));
     }
 }
