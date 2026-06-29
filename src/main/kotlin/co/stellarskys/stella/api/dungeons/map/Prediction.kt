f353c44f4bb0b35a7420b65dea0cf40100734c83
Eclipse-5214 <elijahlipscomb55@gmail.com>
2026-06-29 13:01:59 -0400

    another fix

--- src/main/kotlin/co/stellarskys/stella/api/dungeons/map/Prediction.kt
+++ src/main/kotlin/co/stellarskys/stella/api/dungeons/map/Prediction.kt
@@ -16,8 +16,8 @@ object Prediction {
     }
 
     private val maxRow: Int get() = when(floorNum) {
-        0, 1 -> 3
-        2, 3, 4 -> 4
+        0 -> 3
+        1, 2, 3, 4 -> 4
         else -> 5
     }
 
